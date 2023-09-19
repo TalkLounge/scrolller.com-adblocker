867041324a6b6c73e2b3e71a15f5693eae6267da
TalkLounge <talklounge@yahoo.de>
2023-09-19 20:52:34 +0200

    Fix Popup Blocker by Changing Execution Order

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name            Scrolller.com Adblocker
 // @name:de         Scrolller.com Adblocker
-// @version         1.0.0
+// @version         1.0.1
 // @description     Blocks Ads and the Get Premium Popup
 // @description:de  Blockiert Werbungen und das Get Premium Popup
 // @icon            https://scrolller.com/assets/favicon-16x16.png
@@ -16,21 +16,20 @@
 (function () {
     'use strict';
 
-    const old_window_top_fetch = window.top.fetch;
-
-    window.top.fetch = function(...args) {
-        return new Promise(async (resolve, reject) => {
-            if (args[1].body.indexOf("AffiliateQuery") != -1) {
-                reject();
-            } else {
-                resolve(await old_window_top_fetch.apply(null, args));
-            }
-        });
-    };
-
-	const s = document.createElement("style");
-	s.innerHTML = `.popup {
+    const s = document.createElement("style");
+    s.innerHTML = `.popup {
 		display: none;
 	}`;
-	document.head.append(s);
+    document.head.append(s);
+
+
+    const old_window_top_fetch = window.top.fetch;
+
+    window.top.fetch = function (...args) {
+        if (args[1].body.indexOf("AffiliateQuery") != -1) {
+            return;
+        }
+
+        return old_window_top_fetch.apply(null, args);
+    };
 })();
